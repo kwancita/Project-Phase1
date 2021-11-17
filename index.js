cd29893ff6510bb0bdc80dd493142baa6e49e2a6
Wasanta <kwan_u_day@hotmail.com>
2021-11-16 20:01:40 -0500

    commit

--- index.js
+++ index.js
@@ -6,15 +6,43 @@ const config = {
   }
   fetch(url, config)
   .then(r => r.json())
-  .then(data => joke(data))
+  .then(data => renderJoke(data))
 
   
- 
-  function joke(jokes){
+  const jokeContent = document.getElementById('joke-content')
+  const laughBtn = document.getElementById('emoji-laugh')
+  const rollEyesBtn = document.getElementById('emoji-rollEyes')
+  const laughNum = document.getElementById('laugh-count')
+  const rollEyesNum = document.getElementById('rollEyes-count')
+
+  function renderJoke(jokes){
     // create element
-    const jokeContent = document.getElementById('joke-content')
-    jokeContent.textContent = jokes.joke;
+    jokeContent.textContent = jokes.joke;  
+    laughNum.textContent = 0
+    laughBtn.addEventListener('click', () => addlaugh(laughNum)); 
+    rollEyesBtn.addEventListener('click', () => addRollEyes(rollEyesNum))
   }
+
+  function addlaugh(laughNum){
+    laughNum.textContent = 1+parseInt(laughNum.textContent)
+  }
+  function addRollEyes(rollEyesNum){
+    rollEyesNum.textContent = 1+parseInt(rollEyesNum.textContent)
+  }
+
+
+
+
+  // random button
+  // const randomBtn = document.getElementById('random-btn');
+  // randomBtn.addEventListener('click',getRandomIntInclusive())
+
+  // function getRandomIntInclusive(min, max) {
+  //   min = Math.ceil(min);
+  //   max = Math.floor(max);
+  //   return Math.floor(Math.random() * (max - min + 1)) + min;
+  // }
+
 
 
 
