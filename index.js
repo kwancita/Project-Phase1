3dacefb9f84ee63e40585004125f3b5631e2d6d8
Wasanta <kwan_u_day@hotmail.com>
2021-11-16 21:54:53 -0500

    get random button woork

--- index.js
+++ index.js
@@ -4,44 +4,50 @@ const config = {
     "Content-Type": "application/json", 
     Accept: "application/json" } 
   }
-  fetch(url, config)
-  .then(r => r.json())
-  .then(data => renderJoke(data))
+
+ 
 
   
-  const jokeContent = document.getElementById('joke-content')
-  const laughBtn = document.getElementById('emoji-laugh')
-  const rollEyesBtn = document.getElementById('emoji-rollEyes')
-  const laughNum = document.getElementById('laugh-count')
-  const rollEyesNum = document.getElementById('rollEyes-count')
-
-  function renderJoke(jokes){
-    // create element
-    jokeContent.textContent = jokes.joke;  
-    laughNum.textContent = 0
-    laughBtn.addEventListener('click', () => addlaugh(laughNum)); 
-    rollEyesBtn.addEventListener('click', () => addRollEyes(rollEyesNum))
-  }
-
-  function addlaugh(laughNum){
-    laughNum.textContent = 1+parseInt(laughNum.textContent)
-  }
-  function addRollEyes(rollEyesNum){
-    rollEyesNum.textContent = 1+parseInt(rollEyesNum.textContent)
-  }
+const jokeContent = document.getElementById('joke-content')
+const laughBtn = document.getElementById('emoji-laugh')
+const rollEyesBtn = document.getElementById('emoji-rollEyes')
+const laughNum = document.getElementById('laugh-count')
+const rollEyesNum = document.getElementById('rollEyes-count')
+const random = document.getElementById('random-btn')
 
 
+//function
+function getJoke(){
+  fetch(url, config)
+  .then(data => data.json())
+  .then(dataObj => renderJoke(dataObj))
+}
+
+function renderJoke(jokes){
+  jokeContent.innerHTML = jokes.joke; 
+  laughNum.textContent = 0;
+  rollEyesNum.textContent = 0;
+}
+
+function addlaugh(laughNum){
+  laughNum.textContent = 1+parseInt(laughNum.textContent)
+}
+
+function addRollEyes(rollEyesNum){
+  rollEyesNum.textContent = 1+parseInt(rollEyesNum.textContent)
+}
+
+//evenlistener
+random.addEventListener('click',getJoke);
+
+laughBtn.addEventListener('click', () => addlaugh(laughNum)); 
+
+rollEyesBtn.addEventListener('click', () => addRollEyes(rollEyesNum));
 
 
-  // random button
-  // const randomBtn = document.getElementById('random-btn');
-  // randomBtn.addEventListener('click',getRandomIntInclusive())
 
-  // function getRandomIntInclusive(min, max) {
-  //   min = Math.ceil(min);
-  //   max = Math.floor(max);
-  //   return Math.floor(Math.random() * (max - min + 1)) + min;
-  // }
+
+
 
 
 
